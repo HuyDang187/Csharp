500dd68ba04da7faa58ebbf164ead56990e44a23
HuyDang187 <101924042+HuyDang187@users.noreply.github.com>
2022-06-25 02:21:26 +0700

    Huy

---
+++ MyCsharp/FirstProject/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstProject
+{
+    class Customer
+    {
+        private string firstName;
+        private string lastName;
+
+        public string GetFullName(string firstName , string lastName )
+        {
+            return lastName +" "+ firstName ;
+        }
+    }
+}
---
+++ MyCsharp/FirstProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            
+            Console.WriteLine("Full name : {0}",customer.GetFullName("Huy","Dang"));
+        }
+    }
+}
